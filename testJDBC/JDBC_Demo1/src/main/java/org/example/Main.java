01f91fbf5b47bd1c8ef4f768d9def92005ff42d4
Y.柒 <1939566682@qq.com>
2026-03-14 21:11:10 +0800

    完成了MySQL,无代码推送；新开了JDBC课程，新建项目，暂无代码

---
+++ testJDBC/JDBC_Demo1/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * @author Yang QingBo
+ * {@code @date} 2026-03-14 15:57
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
