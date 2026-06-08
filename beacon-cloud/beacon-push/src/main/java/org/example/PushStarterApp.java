dd2b49c861d8a0928149493e9e13bbdbd7cc6c03
Y.柒 <1939566682@qq.com>
2026-06-08 17:20:09 +0800

    推送模块-搭建推送模块

---
+++ beacon-cloud/beacon-push/src/main/java/org/example/PushStarterApp.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * PushStarterApp
+ *
+ * @author Yang QingBo
+ * @date 2026-06-08 17:04
+ * @description
+ */
+
+@SpringBootApplication
+public class PushStarterApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PushStarterApp.class, args);
+	}
+	
+}
