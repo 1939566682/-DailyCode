9eff71075cbf56ab068ea407e6934eca4ee02a2e
Y.柒 <1939566682@qq.com>
2026-06-09 20:04:19 +0800

    短信网关模块-搭建短信网关模块

---
+++ beacon-cloud/beacon-SmsGateway/src/main/java/org/example/SmsGatewayStaterApp.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SmsGatewayStaterApp
+ *
+ * @author Yang QingBo
+ * @date 2026-06-09 19:50
+ * @description
+ */
+
+@SpringBootApplication
+public class SmsGatewayStaterApp {
+	public static void main(String[] args) {
+		SpringApplication.run(SmsGatewayStaterApp.class, args);
+	}
+}
