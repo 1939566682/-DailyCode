2dcb1486ccbbafbed257950a8eaf708da97c54bd
Y.柒 <1939566682@qq.com>
2026-06-15 09:48:41 +0800

    后台管理模块-搭建后台管理模块

---
+++ beacon-cloud/beacon-webmaster/src/main/java/org/example/WebMasterStarterApp.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * WebMasterStarterApp
+ *
+ * @author Yang QingBo
+ * @date 2026-06-15 09:41
+ * @description
+ */
+
+@SpringBootApplication
+public class WebMasterStarterApp {
+	public static void main(String[] args) {
+		SpringApplication.run(WebMasterStarterApp.class, args);
+	}
+}
